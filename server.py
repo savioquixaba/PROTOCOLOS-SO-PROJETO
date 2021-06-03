392a93198f502c921ab7340029f73e7d98041f67
henrique15775 <61325756+henrique15775@users.noreply.github.com>
2021-06-03 16:47:54 -0300

    Update server.py

--- server.py
+++ server.py
@@ -13,76 +13,84 @@ TRATA AS MENSAGENS RECEBIDAS DO CHAT, FUNCIONA COMO O MEDIADOR DO CHAT.
 
 import socket
 import threading
-import json
 import os
-from configs import a
-import sys
 import time
+import datetime
 TAM_MSG = 1024
-cor = a["estilo"]["cor_fonte"]
 
 
-def convert_data_to_ints(data, big_endian=True):
-    int_count = len(data) // 4  # Assuming uint is 4 bytes long !!!
-    fmt = ">" if big_endian else "<"
-    fmt += "I" * int_count
-    return struct.unpack(fmt, data[:int_count * 4])
 
-def sendata(sock,nome,host,port):
-	
+def envia_mensagem(sock,client,msg):
+	sock.sendto(msg,client)
+
+def envia_arq(msg):
+	print("FUNFOU")
+	sock_file = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock_file.bind(('127.0.1.1', 8000)) 
+	sock_file.listen(1)
+	time.sleep(2)
 	while True:
-		msg_to_send = input()
-		if msg_to_send[0:10] == "/DOWNLOAD ":
-			soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-			sock.sendto(msg_to_send.encode(),(host,port))
-			time.sleep(1)
-			soc.connect((host,8000))
-			soc.send(str.encode(msg_to_send))
-			print("MSG ENVIADA")
-			dados = soc.recv(102400000)
-			nome_arq = msg_to_send[10:]
-			#print('Recebendo:', nome_arq)
-			if dados.decode() =="Arquivo não existe!!":
-				soc.close()
-			else:	
-				arq = open(nome_arq, "wb")
-				arq.write(dados)
+			con, addr = sock_file.accept() 
+			print(addr)
+			print(con)
+			nome_arq = (msg[10:].decode())
+			print('Arquivo solicitado:', nome_arq)
+			try:		
+				status_arq = os.stat(nome_arq)
+				#con.send(str.encode('+OK \n'))
+				
+				arq = open(nome_arq, "rb")
+				x = 1
+				#while x==1 :
+				print("LEITURA")
+				dados = arq.read()
+				#if not dados:
+				#x = 0 
+				con.send(dados)
+				#break
+				
+				print("Fechando...")
 				arq.close()
-				soc.close()
+				con.close()
+				sock_file.close()
+				break
+			except FileNotFoundError:
+				print("Arquivo não encontrado!!")
+				msg = "Arquivo não existe!!"
+				con.send(msg.encode())
+				sock_file.close()
+				con.close()
+			break
 
-		else:		
-			m = cor+f"{nome}// " + msg_to_send +"\033[0;0m"
-			sock.sendto(m.encode(),(host,port)) 
-			time.sleep(1.5)				
-		
-
-
-
-
-
-
-
-def recvdata(sock,vet):
-	
+def recebe_mensagem(udp,clientess= []):
 	while True:
-		os.system("clear||cls")
-		for men in vetor:
-			print(men)
-		msg, recpt = sock.recvfrom(1024000)
-		if recpt:
-			#print(f"{msg.decode()}") 
-			vet.append(msg.decode())
+		history = open("historico.txt","a")
+		msg,cliente = udp.recvfrom(1024)
+		if not cliente in clientess:
+			clientess.append(cliente)
+			print(f"{cliente} adicionado!!")
+		
+		if msg[0:10].decode().upper() ==  '/DOWNLOAD ':
+			envia_arq(msg)
+		else:
+			print(msg.decode())
+			for x in clientess:
+				threading.Thread(target=envia_mensagem,args=(udp,x,msg)).start()
+			hora_atual = datetime.datetime.now()
+			n = msg[7:-6].decode()
+			history.write(hora_atual.strftime("%c")+"****"+n+"\n" )
+	udp.close()	
+	history.close()
 	
+HOST = socket.gethostbyname(socket.gethostname())  # Endereco IP do Servidor
+PORT = 5000  # Porta que o Servidor está
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+orig = (HOST, PORT)
+#global yourname = input("Digite seu nome -> ")
+udp.bind(orig)
 
-name = input("Digite seu nome -> ")
-host = input("Digite o IP do servidor -> ")
-port = int(input("Digite a porta -> "))
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-vetor = []
-welcome_msg = "***************CHAT DOS PROGRAMADORES**********************"
-vetor.append(welcome_msg)
-b = threading.Thread(target=recvdata, args=(s,vetor,))
-b.start()
-a = threading.Thread(target=sendata, args=(s,name,host,port,))
+print(f' {HOST} Servidor no ar...')
+clientess = []
+a = threading.Thread(target=recebe_mensagem,args=(udp,clientess,))
 a.start()
 
