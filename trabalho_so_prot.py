bf72ec3efb5cc0cf6f8c177aa76c43e1340b5f2f
henrique15775 <61325756+henrique15775@users.noreply.github.com>
2021-06-03 02:21:14 -0300

    Update trabalho_so_prot.py

--- trabalho_so_prot.py
+++ trabalho_so_prot.py
@@ -60,37 +60,6 @@ def recebe_mensagem(udp,clientess= []):
 			envia_arq(msg)
 		else:
 			print(msg.decode())
-		
-			"""
-			print("FUNFOU")
-			sock_file = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock_file.bind(('127.0.1.1', 8000)) 
-			sock_file.listen(100)
-			while True:
-				con, addr = sock_file.accept() 
-				print(addr)
-				print(con)
-				time.sleep(10)
-				nome_arq = (msg[10:].decode())
-				print('Arquivo solicitado:', nome_arq)
-				
-				status_arq = os.stat(nome_arq)
-				#con.send(str.encode('+OK \n'))
-				arq = open(nome_arq, "rb")
-				x = 1
-				#while x==1 :
-				print("LEITURA")
-				dados = arq.read()
-				#if not dados:
-				#x = 0 
-				con.send(dados)
-				#break
-				break
-			print("Fechando...")
-			arq.close()
-			con.close()
-			sock_file.close()
-			"""
 			for x in clientess:
 				threading.Thread(target=envia_mensagem,args=(udp,x,msg)).start()
 	udp.close()	
@@ -106,78 +75,4 @@ print(f' {HOST} Servidor no ar...')
 clientess = []
 a = threading.Thread(target=recebe_mensagem,args=(udp,clientess,))
 a.start()
-
-#threading.Thread(target=envia_mensagem).start() 
-"""
-while True:
-	msg,cliente = udp.recvfrom(1024)
-	if not cliente in clientess:
-		clientess.append(cliente)
-		print(f"{cliente} adicionado!!")
-	print(msg.decode())
-	if msg[0:10].decode().upper() ==  '/DOWNLOAD ':
-		print("FUNFOU")
-		sock_file = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock_file.bind(('127.0.1.1', 8000)) 
-		sock_file.listen(100)
-		while True:
-			con, addr = sock_file.accept() 
-			print(addr)
-			print(con)
-			nome_arq = (msg[10:].decode())
-			print('Arquivo solicitado:', nome_arq)
-			
-			status_arq = os.stat(nome_arq)
-			#con.send(str.encode('+OK \n'))
-			arq = open(nome_arq, "rb")
-			x = 1
-			#while x==1 :
-			print("LEITURA")
-			dados = arq.read()
-			#if not dados:
-			#x = 0 
-			con.send(dados)
-			#break
-			break
-		print("Fechando...")
-		arq.close()
-		con.close()
-		sock_file.close()
-		
-	for x in clientess:
-		threading.Thread(target=envia_mensagem,args=(udp,x,msg)).start()
-"""    
-      # quantidade de bytes que espera receber
-    
-    #print( msg.decode()) # decode = de bytes para string
-    #a = input("-> ")
-    #udp.sendto(a.encode(),cliente)
-#udp.close()
-
-
-"""
-from threading import Thread,get_ident
-
-
-class Somador(Thread):
-
-    def __init__(self, inicio, fim):
-        Thread.__init__(self)
-        self.inicio = inicio
-        self.fim = fim
-        self.somatorio = 0
-
-    def run(self):
-        for i in range(self.inicio, self.fim + 1):
-            self.somatorio += i
-            print("Thread- ",get_ident())
-
-s1 = Somador(0, 10)
-s2 = Somador(11,20)
-s1.start()
-s1.join()
-s2.start()
-s2.join()
-print(s1.somatorio)
-"""
 
