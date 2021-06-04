1106512ff171d9eaeff74713b3af12d4bf37a19e
henrique15775 <61325756+henrique15775@users.noreply.github.com>
2021-06-03 23:25:22 -0300

    Add files via upload

---
+++ cliente_windows.py
@@ -0,0 +1,86 @@
+"""
+**********APLICAÇÃO CLIENTE UDP + TCP *************
+
+COMO FUNCIONA: ELE FUNCIONA SE CONECTANDO A UM SERVIDOR Q VAI TRATAR AS MENSAGENS, RECEBENDO SUA MENSAGEM PARA SI PROPRIO APÓS O ENVIO
+		E PARA OS OUTROS CLIENTES TAMBÉM. O SERVIDOR APENAS RECEBE AS MESNAGENS NORMAIS E ENVIA PARA TODOS
+		
+COMO FUNCIONA O /DOWNLOAD: /DOWNLOAD vai abrir uma conexão TCP, q vai enviar o nome do arquivo de texto solicitado que está no servidor. Se o arquivo não existir,
+			   O servidor o avisa e fecha a conexão TCP.
+
+RESUMO:
+	- MENSAGENS = UDP
+	- DOWNLOAD/ENVIO DE ARQS DE TEXTO = TCP
+
+"""
+import socket
+import threading
+import json
+import os
+from configs import a
+import sys
+import time
+TAM_MSG = 1024
+cor = a["estilo"]["cor_fonte"]
+
+"""
+def convert_data_to_ints(data, big_endian=True):
+    int_count = len(data) // 4  # Assuming uint is 4 bytes long !!!
+    fmt = ">" if big_endian else "<"
+    fmt += "I" * int_count
+    return struct.unpack(fmt, data[:int_count * 4])
+"""
+
+
+def sendata(sock,nome,host,port):
+	
+	while True:
+		msg_to_send = input()
+		if msg_to_send[0:10] == "/DOWNLOAD ":
+			soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			sock.sendto(msg_to_send.encode(),(host,port))
+			time.sleep(1)
+			soc.connect((host,8000))
+			soc.send(str.encode(msg_to_send))
+			print("MSG ENVIADA")
+			dados = soc.recv(102400000)
+			nome_arq = msg_to_send[10:]
+			#print('Recebendo:', nome_arq)
+			if dados.decode("ANSI") =="Arquivo não existe!!":
+				soc.close()
+			else:	
+				arq = open(nome_arq, "wb")
+				arq.write(dados)
+				arq.close()
+				soc.close()
+		elif msg_to_send == "quit":
+			quit()
+		else:		
+			m = cor+f"{nome}// " + msg_to_send +"\033[0;0m"
+			sock.sendto(m.encode(),(host,port)) 
+			time.sleep(1.5)				
+		
+#OneDrive\Área de Trabalho\chat
+def recvdata(sock,vet):
+	sock.bind((socket.gethostbyname(socket.gethostname()),9000))
+	while True:
+		os.system("clear||cls")
+		for men in vetor:
+			print(men)
+		msg, recpt = sock.recvfrom(1024)
+		if recpt:
+			print(f"{msg.decode()}") 
+			vet.append(msg.decode())
+	sock.close()
+
+name = input("Digite seu nome -> ")
+host = input("Digite o IP do servidor -> ")
+port = int(input("Digite a porta -> "))
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s_rcv = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+vetor = []
+welcome_msg = "***************CHAT DOS PROGRAMADORES**********************"
+vetor.append(welcome_msg)
+b = threading.Thread(target=recvdata, args=(s_rcv,vetor,))
+b.start()
+a = threading.Thread(target=sendata, args=(s,name,host,port,))
+a.start()
---
+++ server_windows.py
@@ -0,0 +1,96 @@
+"""
+***********SERVIDOR UDP + TCP **************
+
+TRATA AS MENSAGENS RECEBIDAS DO CHAT, FUNCIONA COMO O MEDIADOR DO CHAT.
+
+
+**********SE RECEBER UMA MENSAGEM COMUM, ELE PRINTA E ENVIA PARA OS CLIENTES DO VETOR******************
+
+**********NO DOWNLOAD ELE ABRE UMA CONEXAO TCP PARA ENVIO DE ARQUIVOS DE TEXTO****************
+
+
+"""
+
+import socket
+import threading
+import os
+import time
+import datetime
+TAM_MSG = 1024
+
+
+
+def envia_mensagem(sock,client,msg):
+	sock.sendto(msg,(client[0],9000))
+
+def envia_arq(msg):
+	print("FUNFOU")
+	sock_file = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock_file.bind((socket.gethostbyname(socket.gethostname()), 8000)) 
+	sock_file.listen(100)
+	
+	while True:
+			con, addr = sock_file.accept() 
+			print(addr)
+			print(con)
+			nome_arq = (msg[10:].decode())
+			print('Arquivo solicitado:', nome_arq)
+			try:		
+				status_arq = os.stat(nome_arq)
+				#con.send(str.encode('+OK \n'))
+				
+				arq = open(nome_arq, "rb")
+				x = 1
+				#while x==1 :
+				print("LEITURA")
+				dados = arq.read()
+				#if not dados:
+				#x = 0 
+				con.send(dados)
+				#break
+				
+				print("Fechando...")
+				arq.close()
+				con.close()
+				sock_file.close()
+				break
+			except FileNotFoundError:
+				print("Arquivo não encontrado!!")
+				msg = "Arquivo não existe!!"
+				con.send(msg.encode())
+				sock_file.close()
+				con.close()
+			break
+
+def recebe_mensagem(udp,clientess= []):
+	while True:
+		history = open("historico.txt","a")
+		msg,cliente = udp.recvfrom(1024)
+		if not cliente in clientess:
+			clientess.append(cliente)
+			print(f"{cliente}+++{cliente[1]} adicionado!!")
+		
+		if msg[0:10].decode().upper() ==  '/DOWNLOAD ':
+			envia_arq(msg)
+		else:
+			print(msg.decode())
+			for x in clientess:
+				threading.Thread(target=envia_mensagem,args=(udp,x,msg)).start()
+			hora_atual = datetime.datetime.now()
+			n = msg[7:-6].decode()
+			history.write(hora_atual.strftime("%c")+"****"+n+"\n" )
+	udp.close()	
+	history.close()
+	
+HOST = socket.gethostbyname(socket.gethostname())  # Endereco IP do Servidor
+PORT = 5000  # Porta que o Servidor está
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+orig = (HOST, PORT)
+#global yourname = input("Digite seu nome -> ")
+udp.bind(orig)
+
+print(f' {HOST} Servidor no ar...')
+clientess = []
+a = threading.Thread(target=recebe_mensagem,args=(udp,clientess,))
+a.start()
+
