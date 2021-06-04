6e7e2033a6f41d4a979e2fe5cd71c722c6d21266
henrique15775 <61325756+henrique15775@users.noreply.github.com>
2021-06-04 04:03:01 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -22,14 +22,12 @@ import time
 TAM_MSG = 1024
 cor = a["estilo"]["cor_fonte"]
 
-"""
+
 def convert_data_to_ints(data, big_endian=True):
     int_count = len(data) // 4  # Assuming uint is 4 bytes long !!!
     fmt = ">" if big_endian else "<"
     fmt += "I" * int_count
     return struct.unpack(fmt, data[:int_count * 4])
-"""
-
 
 def sendata(sock,nome,host,port):
 	
@@ -38,30 +36,36 @@ def sendata(sock,nome,host,port):
 		if msg_to_send[0:10] == "/DOWNLOAD ":
 			soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 			sock.sendto(msg_to_send.encode(),(host,port))
-			time.sleep(1)
-			soc.connect((host,8000))
-			soc.send(str.encode(msg_to_send))
-			print("MSG ENVIADA")
-			dados = soc.recv(102400000)
-			nome_arq = msg_to_send[10:]
-			#print('Recebendo:', nome_arq)
-			if dados.decode() =="Arquivo não existe!!":
-				soc.close()
-			else:	
-				arq = open(nome_arq, "wb")
-				arq.write(dados)
-				arq.close()
+			try:
+				
+				time.sleep(1)
+				soc.connect((host,30000))
+				soc.send(str.encode(msg_to_send))
+				print("MSG ENVIADA")
+				dados = soc.recv(1024000)
+				nome_arq = msg_to_send[10:]
+				#print('Recebendo:', nome_arq)
+				if dados.decode() =="Arquivo não existe!!":
+					soc.close()
+				else:	
+					arq = open(nome_arq, "wb")
+					arq.write(dados)
+					arq.close()
+					soc.close()
+			except ConnectionRefusedError:
+				print("CONEXAO RECUSADA")
 				soc.close()
 
 		else:		
 			m = cor+f"{nome}// " + msg_to_send +"\033[0;0m"
 			sock.sendto(m.encode(),(host,port)) 
-			time.sleep(1.5)				
-		
+							
+
 
 def recvdata(sock,vet):
 	
 	while True:
+		
 		os.system("clear||cls")
 		for men in vetor:
 			print(men)
